a532dc64a8bd071353e64379274ec662aeea550d
leeminze214 <61095332+leeminze214@users.noreply.github.com>
2020-07-18 07:57:11 -0400

    fetch time added

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const Discord = require('discord.js');
 //requesting discord.js that will be named discord
-/*fsf*/
 const fetch = require('node-fetch');
 const client = new Discord.Client();
 //this will be the"bot
 const auth = require('./auth.json');
 //getting auth.json from directory which will be named
 const signature = auth.signature;
-
-//getting sig from auth.json
+//getting sig and iex token from auth.json
 const iextoken = auth.iextoken;
 
 client.on("ready", () => {
@@ -25,31 +23,32 @@ client.on("message", (msg) => {
 
     if (refined.startsWith("articles")){
       var stock = msg.content.substring(signature.length+"articles".length).trim()
-      console.log(stock)
       if (stock != ""){
-        articles(msg,stock)}
+        articles(msg,stock)
+      }
     }
   }
 });
 async function articles(msg,stock){
   try{
   var name = stock.toUpperCase();
-  var req = 'https://cloud.iexapis.com/stable/stock/'+stock+'/news/last/5?token='+iextoken
+  var req = 'https://cloud.iexapis.com/stable/stock/'+stock+'/news/last/6?token='+iextoken
+  start_time = new Date().getTime();
   var links = await fetch(req);
+  var time = 'This result was fetched in '+ (new Date().getTime() - start_time) +"ms";
   var real_links = await links.json();
 }catch{
   msg.reply("Sorry, invalid ticker symbol!");
-}
-
+};
   var priorities = {"MarketWatch":[],"InvestorPlace":[], "Business Insider":[]};
   var embed = new Discord.MessageEmbed();
   embed.setTitle("Articles for "+name);
   embed.setColor(0x34b7eb);
-  filter(embed,real_links,priorities);
-msg.reply(embed)
+  filter(embed,real_links,priorities,time);
+  msg.reply(embed);
 };
 
-function filter(embed,real_links,priorities){
+function filter(embed,real_links,priorities,time){
   var count = 0;
   for (i = real_links.length-1; i > 0; i-= 1) {
     if (Object.keys(priorities).includes(real_links[i].source) && real_links[i].lang == "en" && count < 3){
@@ -65,9 +64,9 @@ function filter(embed,real_links,priorities){
       value:"[ Press here ](" + real_links[i].url + ")"
     });
     embed.setThumbnail(real_links[i].image);
-    console.log(real_links[i].headline);
     count++;
     }
-  }
+  };
+  embed.setFooter(time);
 };
 client.login(auth.discordToken);
